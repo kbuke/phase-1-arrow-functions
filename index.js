0c058fa22239748ea939cf618c575ac17ebfde8b
kabuke <kabuke13@gmail.com>
2023-07-12 18:30:03 +0100

    Lab Changes

---
+++ index.js
@@ -0,0 +1,8 @@
+// function divide(num1, num2){
+//     return (num1 / num2)
+// }
+const divide = (num1, num2) => (num1 / num2)
+
+const square = num1 => (num1 **2)
+
+const add = (num1, num2) => (num1 + num2)
